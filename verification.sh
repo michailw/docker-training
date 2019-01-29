a89cf2ef29d1281821a80b0b8e2b5540662c5292
Michał Wójtowicz <michal.wojtowicz@pb.com>
2019-01-29 15:50:46 +0100

    Added MSYS_NO_PATHCONV=1 variable

--- verification.sh
+++ verification.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This variable fixes mounts on Windows when user runs script with git-bash
+MSYS_NO_PATHCONV=1
+
 LOGFILE="verification_"$(date +%Y-%m-%d_%H:%M:%S)".log"
 echo "Welcome in verification script"
 echo "If we find any issues they will be logged into file ${LOGFILE}"
